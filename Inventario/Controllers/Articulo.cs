58cb1db418d5357c273017498cfbe7b819e7bb0a
llopez <ludinlopes@gmail.com>
2025-11-26 17:04:37 -0600

    dsv

--- Inventario/Controllers/Articulo.cs
+++ Inventario/Controllers/Articulo.cs
@@ -8,7 +8,35 @@ namespace Inventario.Controllers
     {
         public IActionResult Articulo_()
         {
+            //return RedirectToAction("VLogin", "Login");
             return View();
+        }
+
+        public IActionResult viewItemEditIt(string idItem, string Tipo) {
+
+            switch (Tipo)
+            {
+                case "Computadora":
+
+                    ImComputadora emple = new ImComputadora();
+
+                    MComputadora a = emple.getEmple(b);
+                    var c = new ImEmpleado();
+                    a.Empleados = c.getEmpleados();
+
+
+                    break;
+                case "2":
+                    ViewBag.Tipo = "Servicio";
+                    break;
+                case "3":
+                    ViewBag.Tipo = "Paquete";
+                    break;
+                default:
+                    ViewBag.Tipo = "Articulo";
+                    break;
+            }
+            return RedirectToAction("VLogin", "Login");
         }
 
     }
