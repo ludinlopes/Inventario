2b1e1598e2c5bf1f7a0ed1d58fa194e319811956
HectorMartinez <hectorabel1992@gmail.com>
2025-11-26 10:49:12 -0600

    cambios de login para que corra

--- Inventario/Controllers/Login.cs
+++ Inventario/Controllers/Login.cs
@@ -76,33 +76,3 @@ namespace Inventario.Controllers
 }
 
 
-using Inventario.Implement;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Inventario.Controllers
-{
-    public class Login : Controller
-    {
-        public IActionResult VLogin()
-        {
-
-            return View();
-        }
-
-        public IActionResult Acceso(string Usuario, string Contraseña, string Sucursal)
-        {
-            HttpContext.Session.SetString("Sucursal EJEM", Sucursal);
-            var a = new ImLogin();
-            var b = a.getUsuario(Usuario, Contraseña);
-            if (b == true)
-            {
-                return RedirectToAction("Home_", "Home");
-            }
-            else
-            {
-                return RedirectToAction("VLogin", "Login");
-            }
-
-        }
-    }
-}
